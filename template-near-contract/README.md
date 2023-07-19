97d1a792342c794e5d45ad84a4af83de9cf0a7f1
Le Khac Thanh Tung <lekhacthanhtung.it@gmail.com>
2023-07-19 16:15:26 +0700

    done all requirements except payment method

---
+++ template-near-contract/README.md
@@ -0,0 +1,65 @@
+## Remove neardev folder
+```
+cargo make clean
+```
+
+## build and deploy
+```
+cargo make dev-deploy
+```
+
+## init contract
+```
+cargo make call-self init
+```
+
+## create new job
+```
+cargo make call create_job '{"min_salary": 500, "max_salary": 1000, "number_job_vacancies": 10, "company_address": "HCM", "jd": "Blockchain Dev"}' --account-id thanhtung2410.testnet
+```
+
+## view all jobs
+```
+cargo make view view_all_jobs
+```
+
+## view job by id
+```
+cargo make view view_job_by_id '{"id": 1}'
+```
+
+## update job
+```
+cargo make call update_job '{"id": 1, "min_salary"
+ : 600, "max_salary": 1200, "number_job_vacancies": 15, "company_address": "HN", "jd": "Rust Dev"}' --account-id thanhtung2410.testnet
+```
+
+ ## register client
+```
+cargo make call register_client '{"name": "Nguyen Van A", "company": "FPT", "phone": "0215201592"}' --account-id thanhtung2410.testnet
+```
+
+ ## view all clients
+```
+cargo make view view_all_clients
+```
+
+ ## register applicant
+```
+cargo make call register_applicant '{"name": "Le Thi B", "cv": "link to Le Thi B cv", "phone": "0321951821"}' --account-id thanhtung2410.testnet
+```
+
+## view all applicants
+```
+cargo make view view_all_applicants
+```
+
+## take job
+```
+cargo make call take_job '{"id": 1}' --account-id thanhtung2410.testnet
+```
+
+## view all job taken
+```
+cargo make view view_all_jobs_taken
+```
--- template-near-contract/src/lib.rs
+++ template-near-contract/src/lib.rs
@@ -1,60 +1,287 @@
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::{near_bindgen, AccountId, Balance, PanicOnDefault, Promise};
+use near_sdk::collections::{LookupMap, UnorderedMap};
+use near_sdk::serde::{Deserialize, Serialize};
+use near_sdk::{env, near_bindgen, AccountId, Balance, PanicOnDefault, Promise};
+
+pub type JobId = u128;
+pub type ClientId = AccountId;
+pub type ApplicantId = AccountId;
 
 // Define the contract structure
 #[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize, PanicOnDefault)]
 pub struct Contract {
-  pub owner_id: AccountId,
+    pub owner_id: AccountId,
+
+    // Job
+    pub job_by_id: LookupMap<JobId, Job>,
+    pub all_jobs: UnorderedMap<u128, Job>,
+    pub total_jobs: u64,
+
+    // Client
+    pub client_by_id: LookupMap<ClientId, Client>,
+    pub all_clients: UnorderedMap<u128, Client>,
+    pub total_clients: u64,
+
+    // Applicant
+    pub applicant_by_id: LookupMap<ApplicantId, Applicant>,
+    pub all_applicants: UnorderedMap<u128, Applicant>,
+    pub total_applicants: u64,
+
+    pub jobs_per_client: UnorderedMap<ClientId, Vec<Job>>,
+
+    pub jobs_apply_by_applicants: UnorderedMap<ApplicantId, Job>,
 }
 
-pub struct Job {}
+#[derive(BorshDeserialize, BorshSerialize, Deserialize, Serialize, Clone)]
+#[serde(crate = "near_sdk::serde")]
+pub struct Job {
+    id: JobId,
+    min_salary: u64,
+    max_salary: u64,
+    number_job_vacancies: i8,
+    company_address: String,
+    jd: String,
+    client_id: AccountId,
+}
+
+#[derive(BorshDeserialize, BorshSerialize, Deserialize, Serialize)]
+#[serde(crate = "near_sdk::serde")]
+pub struct Client {
+    id: ClientId,
+    name: String,
+    company: String,
+    phone: String,
+}
+
+#[derive(BorshDeserialize, BorshSerialize, Deserialize, Serialize)]
+#[serde(crate = "near_sdk::serde")]
+pub struct Applicant {
+    id: ApplicantId,
+    name: String,
+    cv: String,
+    phone: String,
+}
 
 pub trait OutSourcing {
-  // Đăng ký làm freelancer.
-  fn register_executor();
-  // Đăng ký để làm người giao job.
-  fn register_client();
-  // Client -> Tạo Jobs
-  fn create_job();
-  // Freelancer -> Take.
-  fn take_job();
-  // Update
-  fn update_job();
-  // Payment
-  fn payment(price: Balance) -> Promise;
-  // View
-  fn view_all_jobs() -> Vec<String>;
-  fn view_job_by_id();
-}
+    // Đăng ký làm người xin việc
+    fn register_applicant(&mut self, name: String, cv: String, phone: String);
 
-// Nhớ là phân insert,
+    // Đăng ký để làm người tạo job
+    fn register_client(&mut self, name: String, company: String, phone: String);
+
+    fn view_all_clients(&self) -> Vec<Client>;
+
+    fn create_job(
+        &mut self,
+        min_salary: u64,
+        max_salary: u64,
+        number_job_vacancies: i8,
+        company_address: String,
+        jd: String,
+    );
+
+    // Applicant -> Take
+    fn take_job(&mut self, id: JobId);
+
+    // Update
+    fn update_job(
+        &mut self,
+        id: JobId,
+        min_salary: u64,
+        max_salary: u64,
+        number_job_vacancies: i8,
+        company_address: String,
+        jd: String,
+    ) -> Job;
+
+    // Payment
+    fn payment(price: Balance) -> Promise;
+
+    // View
+    fn view_all_jobs(&self) -> Vec<Job>;
+    fn view_job_by_id(&self, id: JobId) -> Job;
+    fn view_all_applicants(&self) -> Vec<Applicant>;
+    fn view_all_jobs_taken(&self) -> Vec<(AccountId, Job)>;
+}
 
 // Implement the contract structure
 #[near_bindgen]
+impl Contract {
+    #[init]
+    pub fn init() -> Self {
+        Self {
+            owner_id: env::signer_account_id(),
+
+            job_by_id: LookupMap::new(b"job by id".try_to_vec().unwrap()),
+            all_jobs: UnorderedMap::new(b"all jobs".try_to_vec().unwrap()),
+            total_jobs: 0,
+
+            client_by_id: LookupMap::new(b"client by id".try_to_vec().unwrap()),
+            all_clients: UnorderedMap::new(b"all clients".try_to_vec().unwrap()),
+            total_clients: 0,
+
+            applicant_by_id: LookupMap::new(b"applicant by id".try_to_vec().unwrap()),
+            all_applicants: UnorderedMap::new(b"all applicants".try_to_vec().unwrap()),
+            total_applicants: 0,
+
+            jobs_per_client: UnorderedMap::new(b"jobs per client".try_to_vec().unwrap()),
+
+            jobs_apply_by_applicants: UnorderedMap::new(
+                b"jobs apply by applicants".try_to_vec().unwrap(),
+            ),
+        }
+    }
+}
+
+#[near_bindgen]
 impl OutSourcing for Contract {
-  fn register_executor() {
-    todo!()
-  }
-  fn register_client() {
-    todo!()
-  }
-  fn create_job() {
-    todo!()
-  }
-  fn take_job() {
-    todo!()
-  }
-  fn update_job() {
-    todo!()
-  }
-  fn view_all_jobs() -> Vec<String> {
-    vec![]
-  }
-  fn view_job_by_id() {}
-  fn payment(price: Balance) -> Promise {
-    // Assert!
-    // Price == env::attached_deposit();
-    Promise::new("eamondev.testnet".parse().unwrap()).transfer(price)
-  }
+    fn register_applicant(&mut self, name: String, cv: String, phone: String) {
+        let applicant_id = env::signer_account_id();
+        assert!(
+            !self.applicant_by_id.contains_key(&applicant_id),
+            "You have registered Applicant account before"
+        );
+        self.total_applicants += 1;
+        let applicant = Applicant {
+            id: applicant_id.clone(),
+            name,
+            cv,
+            phone,
+        };
+        self.applicant_by_id.insert(&applicant_id, &applicant);
+        self.all_applicants
+            .insert(&(self.total_applicants as u128), &applicant);
+    }
+
+    fn view_all_applicants(&self) -> Vec<Applicant> {
+        // refactor
+
+        let mut applicants = Vec::new();
+        for ele in self.all_applicants.iter() {
+            applicants.push(ele.1);
+        }
+        applicants
+    }
+
+    fn register_client(&mut self, name: String, company: String, phone: String) {
+        let client_id = env::signer_account_id();
+        assert!(
+            !self.client_by_id.contains_key(&client_id),
+            "You have registered Client account before"
+        );
+        self.total_clients += 1;
+        let client = Client {
+            id: client_id.clone(),
+            name,
+            company,
+            phone,
+        };
+        
+        self.client_by_id.insert(&client_id, &client);
+        self.all_clients
+            .insert(&(self.total_clients as u128), &client);
+    }
+
+    fn view_all_clients(&self) -> Vec<Client> {
+        let mut clients = Vec::new();
+        for ele in self.all_clients.iter() {
+            clients.push(ele.1);
+        }
+        clients
+    }
+
+    fn create_job(
+        &mut self,
+        min_salary: u64,
+        max_salary: u64,
+        number_job_vacancies: i8,
+        company_address: String,
+        jd: String,
+    ) {
+        assert!(
+            self.client_by_id.contains_key(&env::signer_account_id()),
+            "You need to register Client account before post job hiring"
+        );
+        self.total_jobs += 1;
+        let job_id = self.total_jobs;
+        let job = Job {
+            id: job_id as u128,
+            min_salary,
+            max_salary,
+            number_job_vacancies,
+            company_address,
+            jd,
+            client_id: env::signer_account_id(),
+        };
+        let client_id = env::signer_account_id();
+        
+        let mut jobs_set: Vec<Job> = self
+            .jobs_per_client
+            .get(&client_id)
+            .unwrap_or_else(|| Vec::new());
+        jobs_set.push(job.clone());
+
+        self.jobs_per_client.insert(&client_id, &jobs_set);
+        self.job_by_id.insert(&(job_id as u128), &job);
+        self.all_jobs.insert(&(job_id as u128), &job);
+    }
+    
+    fn take_job(&mut self, id: JobId) {
+        let mut job = self.view_job_by_id(id);
+        job.number_job_vacancies -= 1;
+        
+        self.all_jobs.insert(&job.id, &job);
+        self.job_by_id.insert(&job.id, &job);
+
+        self.jobs_apply_by_applicants
+            .insert(&env::signer_account_id(), &job);
+    }
+
+    fn view_all_jobs_taken(&self) -> Vec<(AccountId, Job)> {
+        let mut jobs_taken = Vec::new();
+        for ele in self.jobs_apply_by_applicants.iter() {
+            jobs_taken.push((ele.0, ele.1));
+        }
+        jobs_taken
+    }
+
+    fn update_job(
+        &mut self,
+        id: JobId,
+        min_salary: u64,
+        max_salary: u64,
+        number_job_vacancies: i8,
+        company_address: String,
+        jd: String,
+    ) -> Job {
+        let mut job_found = self.view_job_by_id(id);
+        job_found.min_salary = min_salary;
+        job_found.max_salary = max_salary;
+        job_found.number_job_vacancies = number_job_vacancies;
+        job_found.company_address = company_address;
+        job_found.jd = jd;
+        
+        self.job_by_id.insert(&(id as u128), &job_found);
+        self.all_jobs.insert(&(id as u128), &job_found);
+        
+        job_found
+    }
+
+    fn view_all_jobs(&self) -> Vec<Job> {
+        let mut jobs = Vec::new();
+        for ele in self.all_jobs.iter() {
+            jobs.push(ele.1);
+        }
+        jobs
+    }
+
+    fn view_job_by_id(&self, id: JobId) -> Job {
+        self.all_jobs.get(&id).unwrap()
+    }
+
+    fn payment(price: Balance) -> Promise {
+        // assert!(price == env::attached_deposit());
+        Promise::new("eamondev.testnet".parse().unwrap()).transfer(price)
+    }
 }
